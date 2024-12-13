56d081c2cfb235bbee83b6e3a9e830b7cc933cec
sassybunsthebun <sara.batevik@epost.no>
2024-12-13 19:18:37 +0100

    Added more movement logic

--- main.py
+++ main.py
@@ -2,10 +2,17 @@ import pygame
 from constants import *
 from player import Player
 
+
 def main(): 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) #makes the screen the defined screen size
     clock = pygame.time.Clock() #sets the time clock to ensure proper FPS
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2) #sets the player in the middle of the screen
     dt = 0
     #print("Starting asteroids!")
@@ -17,12 +24,18 @@ def main():
             if event.type == pygame.QUIT:
                 return
             
-        player.update(dt)
-        
+        #player.update(dt)
+        for object in updatable: 
+            object.update(dt)
+
         screen.fill("black")
-        player.draw(screen)
+
+        for object in drawable: 
+            object.draw(screen)
+        #player.draw(screen)
         pygame.display.flip()
 
+        #set the framerate to 60 FPS
         dt = clock.tick(60) / 1000    
 
 if __name__ == "__main__":
