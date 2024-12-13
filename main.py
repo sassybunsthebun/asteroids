9c08da8b3d0eafd41e742f3501b68f02c800a3ca
sassybunsthebun <sara.batevik@epost.no>
2024-12-13 18:28:22 +0100

    Added player graphics

--- main.py
+++ main.py
@@ -1,29 +1,27 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
 import pygame
 from constants import *
+from player import Player
 
 def main(): 
     pygame.init()
-    clock =pygame.time.Clock()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) #makes the screen the defined screen size
+    clock = pygame.time.Clock() #sets the time clock to ensure proper FPS
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2) #sets the player in the middle of the screen
     dt = 0
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-    color = (0,0,0)
+    #print("Starting asteroids!")
+    #print(f"Screen width: {SCREEN_WIDTH}")
+    #print(f"Screen height: {SCREEN_HEIGHT}")
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill(color)
+            
+        screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
 
-        dt = pygame.time.Clock.tick(60) / 1000
-
-    
+        dt = clock.tick(60) / 1000    
 
 if __name__ == "__main__":
     main()
